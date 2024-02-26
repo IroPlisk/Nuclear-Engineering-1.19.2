9da0ab4e5850608ff0bc65ec1ca83b6622fa6142
IroPlisk <loremcrux04@gmail.com>
2024-02-26 01:41:21 +0100

    Aggiunte polveri, pellet e un paio di cosettine

--- src/main/java/com/iroplisk/nuclearengineering/items/BlanketItem.java
+++ src/main/java/com/iroplisk/nuclearengineering/items/BlanketItem.java
@@ -2,33 +2,37 @@ package com.iroplisk.nuclearengineering.items;
 
 import ic2.api.reactor.IReactor;
 import ic2.api.reactor.IReactorComponent;
+import ic2.api.reactor.ISteamReactor;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
 public class BlanketItem extends Item implements IReactorComponent {
     private final Item resultItem;
+    private final int enrichmentDuration;
+
     public BlanketItem(CreativeModeTab tab, Item resultItem, int enrichmentDuration) {
         super(new Properties().tab(tab).setNoRepair().durability(enrichmentDuration));
         this.resultItem = resultItem;
+        this.enrichmentDuration = enrichmentDuration;
     }
 
     @Override
-    public void processChamber(ItemStack itemStack, IReactor iReactor, int i, int i1, boolean b, boolean b1) {
-
+    public void processChamber(ItemStack itemStack, IReactor iReactor, int i, int i1, boolean b, boolean damageTick) {
     }
 
     @Override
-    public boolean acceptUraniumPulse(ItemStack itemStack, IReactor iReactor, ItemStack itemStack1, int i, int i1, int i2, int i3, boolean b, boolean b1) {
-        final int myLevel = itemStack.getDamageValue() - 1 - iReactor.getHeat() / 3000;
-        if (myLevel <= 0) {
-            iReactor.setStackInReactor(i, i1, new ItemStack(resultItem, 1));
-        }
-        else {
-            itemStack.setDamageValue(myLevel);
+    public boolean acceptUraniumPulse(ItemStack itemStack, IReactor iReactor, ItemStack itemStack1, int i, int i1, int i2, int i3, boolean heatRun, boolean damageTick) {
+        if (damageTick) {
+            if (itemStack.getDamageValue() == enrichmentDuration) {
+                iReactor.setStackInReactor(i, i1, new ItemStack(resultItem, 1));
+            } else {
+                itemStack.setDamageValue(1+(iReactor.getHeat() / 3000));
+            }
         }
         return true;
     }
+
 
     @Override
     public boolean canStoreHeat(ItemStack itemStack, IReactor iReactor, int i, int i1) {
--- src/main/java/com/iroplisk/nuclearengineering/items/ItemInit.java
+++ src/main/java/com/iroplisk/nuclearengineering/items/ItemInit.java
@@ -54,5 +54,45 @@ public class ItemInit {
     public static final RegistryObject<Item> BLANKETY = ITEMS.register("blankety",
             () -> new BlanketItem(NuclearEngineeringCreativeModeTab.NUCLEAR_ENGINEERING_CREATIVE_MODE_TAB, ItemInit.BLANKETENRICHEDY.get(), 25000));
 
+    //polveri
+    public static final RegistryObject<Item> DUSTA = ITEMS.register("dusta",
+            () -> new RadioactiveItem(NuclearEngineeringCreativeModeTab.NUCLEAR_ENGINEERING_CREATIVE_MODE_TAB, "alpha", 127));
+    public static final RegistryObject<Item> DUSTCE = ITEMS.register("dustce",
+            () -> new RadioactiveItem(NuclearEngineeringCreativeModeTab.NUCLEAR_ENGINEERING_CREATIVE_MODE_TAB, "gamma", 32150));
+    public static final RegistryObject<Item> DUSTCO = ITEMS.register("dustco",
+            () -> new RadioactiveItem(NuclearEngineeringCreativeModeTab.NUCLEAR_ENGINEERING_CREATIVE_MODE_TAB, "gamma", 44000));
+    public static final RegistryObject<Item> DUSTIO = ITEMS.register("dustio",
+            () -> new RadioactiveItem(NuclearEngineeringCreativeModeTab.NUCLEAR_ENGINEERING_CREATIVE_MODE_TAB, "beta", 46000));
+    public static final RegistryObject<Item> DUSTIR = ITEMS.register("dustir",
+            () -> new RadioactiveItem(NuclearEngineeringCreativeModeTab.NUCLEAR_ENGINEERING_CREATIVE_MODE_TAB, "gamma", 22200));
+    public static final RegistryObject<Item> DUSTM = ITEMS.register("dustm",
+            () -> new RadioactiveItem(NuclearEngineeringCreativeModeTab.NUCLEAR_ENGINEERING_CREATIVE_MODE_TAB, "beta", 629));
+    public static final RegistryObject<Item> DUSTP = ITEMS.register("dustp",
+            () -> new RadioactiveItem(NuclearEngineeringCreativeModeTab.NUCLEAR_ENGINEERING_CREATIVE_MODE_TAB, "alpha", 644));
+    public static final RegistryObject<Item> DUSTSR = ITEMS.register("dustsr",
+            () -> new RadioactiveItem(NuclearEngineeringCreativeModeTab.NUCLEAR_ENGINEERING_CREATIVE_MODE_TAB, "beta", 5217));
+    public static final RegistryObject<Item> DUSTY = ITEMS.register("dusty",
+            () -> new RadioactiveItem(NuclearEngineeringCreativeModeTab.NUCLEAR_ENGINEERING_CREATIVE_MODE_TAB, "beta", 200));
+
+    //pellet
+    public static final RegistryObject<Item> PELLETA = ITEMS.register("pelleta",
+            () -> new RadioactiveItem(NuclearEngineeringCreativeModeTab.NUCLEAR_ENGINEERING_CREATIVE_MODE_TAB, "alpha", 127));
+    public static final RegistryObject<Item> PELLETCE = ITEMS.register("pelletce",
+            () -> new RadioactiveItem(NuclearEngineeringCreativeModeTab.NUCLEAR_ENGINEERING_CREATIVE_MODE_TAB, "gamma", 32150));
+    public static final RegistryObject<Item> PELLETCO = ITEMS.register("pelletco",
+            () -> new RadioactiveItem(NuclearEngineeringCreativeModeTab.NUCLEAR_ENGINEERING_CREATIVE_MODE_TAB, "gamma", 44000));
+    public static final RegistryObject<Item> PELLETIO = ITEMS.register("pelletio",
+            () -> new RadioactiveItem(NuclearEngineeringCreativeModeTab.NUCLEAR_ENGINEERING_CREATIVE_MODE_TAB, "beta", 46000));
+    public static final RegistryObject<Item> PELLETIR = ITEMS.register("pelletir",
+            () -> new RadioactiveItem(NuclearEngineeringCreativeModeTab.NUCLEAR_ENGINEERING_CREATIVE_MODE_TAB, "gamma", 22000));
+    public static final RegistryObject<Item> PELLETM = ITEMS.register("pelletm",
+            () -> new RadioactiveItem(NuclearEngineeringCreativeModeTab.NUCLEAR_ENGINEERING_CREATIVE_MODE_TAB, "beta", 629));
+    public static final RegistryObject<Item> PELLETP = ITEMS.register("pelletp",
+            () -> new RadioactiveItem(NuclearEngineeringCreativeModeTab.NUCLEAR_ENGINEERING_CREATIVE_MODE_TAB, "alpha", 644));
+    public static final RegistryObject<Item> PELLETS = ITEMS.register("pellets",
+            () -> new RadioactiveItem(NuclearEngineeringCreativeModeTab.NUCLEAR_ENGINEERING_CREATIVE_MODE_TAB, "beta", 5217));
+    public static final RegistryObject<Item> PELLETY = ITEMS.register("pellety",
+            () -> new RadioactiveItem(NuclearEngineeringCreativeModeTab.NUCLEAR_ENGINEERING_CREATIVE_MODE_TAB, "beta", 200));
+
     public static void register(IEventBus eventBus) { ITEMS.register(eventBus);}
 }
